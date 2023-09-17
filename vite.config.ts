9e1adac836dabf7193e7c15f8cafb598270395f8
Igor Kashchenko <blackangryrabbit@gmail.com>
2023-09-18 00:28:09 +0300

    api server address change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jsn-test-task/client',
+  base: '/jsn-test-task-client',
   resolve: {
     alias: {
       '@': '/src'
